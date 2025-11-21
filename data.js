ded78895c0f4504d33951de44b82b12fa4c5122e
fsh77 <fsh77@mail.com>
2025-11-21 19:47:44 +0300

    AIDA64 live

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-var sensor = {"cpu": "64", "gpu": "40", "ram": 55, "zaman": "19:47:25", "guncelleme": "19:47:26", "ts": 1763743646066};
+var sensor = {"cpu": "63", "gpu": "40", "ram": 55, "zaman": "19:47:33", "guncelleme": "19:47:34", "ts": 1763743654258};
